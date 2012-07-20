a38f0904e472a66d55435cdd416ea14a85284477
Rick Branson <rick@diodeware.com>
2012-07-20 15:07:18 -0700

    Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = "kombu-multibroker",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Rick Branson",
     author_email = "rick@diodeware.com",
     description = ("Support for multiple brokers in Kombu"),
